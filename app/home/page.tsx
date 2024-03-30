592dd681938b7d1865e5f85350a469a1a3a41692
OriTeicher <oriteicherudemy@gmail.com>
2024-03-31 00:19:19 +0300

    db integration

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 import HomeContent from "@/components/home/HomeContent"
 import HomeIcons from "@/components/home/HomeIcons"
-import React, { useState } from "react"
+import { db } from "@/services/db.service"
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
   const [selectedContent, setSelectedContent] = useState<string>("chat")
